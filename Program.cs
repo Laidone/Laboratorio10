b69d988c6b2ea9c104c7ecdefaca7ec8cbf185bb
Laidone <laidonecarvalho@hotmail.com>
2021-11-19 16:30:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Profram
+{
+    public static void Main(string[] args)
+    {
+        // inicializa o termometro e mostra seu valor
+        TermometroLimite term = new TermometroLimite(5); 
+        Console.WriteLine(term.ToString()); 
+        // adiciona um tratador ao evento LimiteSuperiorEvent
+        term.LimiteSuperiorEvent += new TermometroLimite.MeuDelegate(TrataLimiteSuperior); 
+        // aumentar a temperatura além do limite term.Aumentar(6);
+        term.Aumentar(6);
+        Console.WriteLine(term.ToString());
+        term.Diminuir(2);
+        System.Console.WriteLine(term.ToString());
+
+    }
+    private static void TrataLimiteSuperior(string msg) 
+        { 
+            Console.WriteLine(msg); 
+        }
+}
